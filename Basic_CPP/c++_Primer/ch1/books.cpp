27870b2742b8096e356627b09481b837e86b822f
lambda <642146887@qq.com>
2014-07-16 01:16:03 +0800

    add c++_primer ch1/books.cpp

---
+++ Basic_CPP/c++_Primer/ch1/books.cpp
@@ -0,0 +1,22 @@
+/*
+ * =========================================================
+ *       Filename:  books.cpp
+ *    Description:  某书店以文件形式保存每笔交易,交易记录(ISBN, number, price)
+ *    基于记录来查看销售情况.统计每本书销售册数,总收入等等.
+ *
+ * =========================================================
+ */
+
+
+#include <iostream> // 常用的IO对象 cin, cout.利用重定向,将IO流与文件联系起来
+#include "sales_item.hpp"
+using namespace std;
+int main(){
+/*     std::cout << "input two number:" << endl; //endl 可刷新buffer, 看到写入到out流中的内容
+ *     int v1, v2;
+ *     std::cin >> v1 >> v2;// >> 类比函数 scanf()
+ *     std::cout << "sum is " << v1 + v2 << endl; // std:: 作用域操作符
+ */
+    return 0;
+}
+
