774a00a1d3cd854f244030a7b09c91c34f915ff6
tinylamb <642146887@qq.com>
2015-08-15 13:01:45 +0800

    quick commit

---
+++ Basic_CPP/Principles_Practice/ch2/hello_world.cpp
@@ -0,0 +1,13 @@
+/*
+ * =========================================================
+ *       Filename:  hello_world.cpp
+ *    Description:  
+ *
+ * =========================================================
+ */
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "hello,programming!\nHere we go!\n";
+    return 0;
+}
